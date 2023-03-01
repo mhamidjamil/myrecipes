072cbba54de9e127189e206f9e504f0edc80dbc7
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-03-01 15:16:24 +0500

    lecture_60(final)

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -24,30 +24,33 @@ class ChefTest < ActiveSupport::TestCase
     @chef.email = " "
     assert_not @chef.valid?
   end
+  
   test "email should not be too long" do
-    @chef.email = "a" * 255 + "@example.com"
+    @chef.email = "a" * 245 + "@example.com"
     assert_not @chef.valid?
   end
+  
   test "email should accept correct format" do
-    valid_emails = %w[user@example.com hamid@gmail.com m.hamid@yahoo.ca hamid+jamil@co.uk.org]
+    valid_emails = %w[user@example.com Hamid@gmail.com M.hamid@yahoo.ca hamid+jamil@co.uk.org]
     valid_emails.each do |valids|
       @chef.email = valids
       assert @chef.valid?, "#{valids.inspect} should be valid"
     end
   end
+  
   test "should reject invalid addresses" do
-    invalid_emails = %w[hamid@example hamid@example,com hamid.name@gmail. hamid@jamil+raza.com]
+    invalid_emails = %w[hamid@example hamid@example,com hamid.name@gmail. m@hamid+jamil.com]
     invalid_emails.each do |invalids|
       @chef.email = invalids
       assert_not @chef.valid?, "#{invalids.inspect} should be invalid"
     end
-    test "email should be unique and case insensitive" do
-        duplicate_chef = @chef.dup
-        duplicate_chef.email = @chef.email.upcase
-        @chef.save
-        assert_not duplicate_chef.valid?
-      end
   end 
-
-
-end 
+  
+  test "email should be unique and case insensitive" do
+    duplicate_chef = @chef.dup
+    duplicate_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicate_chef.valid?
+  end
+  
+end
