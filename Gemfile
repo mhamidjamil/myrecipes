cc139fafdccaa7dbc14ed9ce2542a6ec00e28834
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-03-01 18:48:00 +0500

    !complete_71

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ group :development, :test do
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
+gem "rails-controller-testing"
+
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
 
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,8 +1,32 @@
 require "test_helper"
 
 class RecipesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @chef = Chef.create!(chefname: "shoaib", email: "shoaib@example.com")
+    @recipe = Recipe.create(name: "vegetable saute",description: "great vegetable sautee,add vegetable and oil", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken saute",description: "great chicken dish")
+    @recipe2.save
+  end
+
   test "should get recipes index" do
     get recipes_url
     assert_response :success
   end
+
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+
 end
