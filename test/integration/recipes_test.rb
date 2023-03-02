3fd273e16c64c4c0003570fe4b5ac97b11d624bf
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-03-02 14:14:00 +0500

    Section_4_end_(without_styling)

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -27,7 +27,11 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_select 'a[href=?]', recipes_path, text: "Return to recipes listing"
   end
+
   test "create new valid recipe" do
     get new_recipe_path
   end
