148d6c094ba03cbdb8870dcb3115c972287bb92d
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-03-02 16:15:00 +0500

    section_4_ended_with_refactor

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,17 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
 
     def index
         @recipes = Recipe.all
     end
 
     def show
-        @recipe = Recipe.find(params[:id])
     end
+
     def new
         @recipe = Recipe.new
     end
+
     def create
         @recipe = Recipe.new(recipe_params)
         @recipe.chef = Chef.first
@@ -20,12 +22,11 @@ class RecipesController < ApplicationController
           render 'new'
         end
       end
+
       def edit
-        @recipe = Recipe.find(params[:id])
       end
       
       def update
-        @recipe = Recipe.find(params[:id])
         if @recipe.update(recipe_params)
           flash[:success] = "Recipe was updated successfully!"
           redirect_to recipe_path(@recipe)
@@ -42,7 +43,12 @@ class RecipesController < ApplicationController
 
       private
       
-        def recipe_params
-          params.require(:recipe).permit(:name, :description)
-        end
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
+      end
+
+      def recipe_params
+        params.require(:recipe).permit(:name, :description)
+      end
+
 end
