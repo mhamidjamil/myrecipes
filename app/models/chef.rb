414cde70d1844bdc2c856080e14b3d5cc69bb407
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-03-06 12:03:41 +0500

    duplicate_page_bug_solved

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,10 +2,7 @@ class Chef < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :chefname, presence: true, length: { maximum: 30 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: { case_sensitive: false }
-
+    validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
     has_many :recipes, dependent: :destroy
     has_secure_password
